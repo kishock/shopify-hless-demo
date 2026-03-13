12d557f7c418a89799c9b254653bf12a861c6430
kishcok <kishock88@gmail.com>
2026-03-13 00:31:58 -0700

    Update favicon and remove welcome toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { CartProvider } from "components/cart/cart-context";
 import { Navbar } from "components/layout/navbar";
-import { WelcomeToast } from "components/welcome-toast";
 import { GeistSans } from "geist/font/sans";
 import { getCart } from "lib/shopify";
 import { baseUrl } from "lib/utils";
@@ -51,7 +50,6 @@ export default async function RootLayout({
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
           </main>
         </CartProvider>
       </body>
